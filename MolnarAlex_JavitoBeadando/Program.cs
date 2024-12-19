72578e5692d6e882f839d5442807c85e5445e420
MAlex07 <molnar.alex@petrik.hu>
2024-12-19 19:05:10 +0100

    F01 elkészült

--- MolnarAlex_JavitoBeadando/Program.cs
+++ MolnarAlex_JavitoBeadando/Program.cs
@@ -8,9 +8,49 @@ namespace MolnarAlex_JavitoBeadando
 {
     internal class Program
     {
+        static void F01(string[] szavak)
+        {
+            Random r = new Random();
+            
+            
+            
+            for (int i = 0; i < szavak.Length; i++)
+            {
+                szavak[i] = RandomSzavak(r);
+            }
+
+            
+        }
+
+        static string RandomSzavak(Random random)
+        {
+            string betuk = "abcdefghjklmnop";
+            char[] szo = new char[10];
+
+            for (int i = 0;i < 10; i++)
+            {
+                szo[i] = betuk[random.Next(betuk.Length)];
+            }
+
+             return new string(szo);
+            
+
+
+        }
+
+
+
+
         static void Main(string[] args)
         {
+            string[] keszSzavak = new string[15];
+
+            F01(keszSzavak);
             
+            foreach(string szo  in keszSzavak)
+            {
+                Console.WriteLine(szo);
+            }
 
 
 
